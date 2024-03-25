6696533b927a1329b9da7d39ad80dff7aafc3023
fahis <you@example.com>
2024-03-25 12:41:17 +0530

    program without thread

---
+++ threads.py
@@ -0,0 +1,15 @@
+import time 
+
+start = time.perf_counter()
+
+def do_something():
+    print("sleeping 1 second")
+    time.sleep(1)
+    print("done sleeping")
+for i in range(10):
+    do_something()
+
+finish= time.perf_counter()
+
+print(f"finished in :{round(finish-start,2) }second")
+
